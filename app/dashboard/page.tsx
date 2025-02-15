191cd765788ca832f9801910d1c50cebc40866bc
Damian Bulata <inamida16@gmail.com>
2025-02-15 23:25:32 +0200

    Cleaned testing things

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default async function Dashboard() {
+  return (
+    <div>
+      <p>Hello from the dashboard</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link';
-
 export default async function Home() {
   return (
     <div>
-      <p>Hello World!</p>
-      <p>Go to posts:</p>
-      <button className="bg-slate-400 py-5 px-6 rounded-xl">
-        <Link href="/posts">Posts</Link>
-      </button>
+      <p>Hello from Root</p>
     </div>
   );
 }
